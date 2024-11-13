29de19a469311fb4e76abf208c287dfd7badeafd
Yanjar <yanjarappa65@gmail.com>
2024-11-13 20:55:17 +0530

    testing purpose

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+const v = 'string';
+if (true) {
+  consoloe.log('hi');
+}
+app.get('/', (request, response) => {
+  console.log(v);
+});
